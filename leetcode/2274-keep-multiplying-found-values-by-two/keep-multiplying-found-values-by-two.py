3362340ae6f037c30ecb6cd19da1693764239f0f
Erika naidoo <138525770+Erii-15@users.noreply.github.com>
2025-11-23 19:50:24 +0200

    Time: 1 ms (37.74%) | Memory: 12.6 MB (15.89%) - LeetSync

---
+++ leetcode/2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def findFinalValue(self, nums, original):
+        """
+        :type nums: List[int]
+        :type original: int
+        :rtype: int
+        """
+        while(True):
+            if (original in nums):
+                 original=original*2
+            else:
+                break
+        return original
+        
+        
