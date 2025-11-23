ee8e13d9c1970f98c391479336d90249c33b3844
Erika naidoo <138525770+Erii-15@users.noreply.github.com>
2025-11-23 19:23:22 +0200

    Time: 29 ms (46.49%) | Memory: 48.6 MB (75.38%) - LeetSync

---
+++ leetcode/1-two-sum/two-sum.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        for(int x=0;x<nums.Length;x++){
+            for(int y=x+1;y<nums.Length;y++){
+                if(nums[x]+nums[y]==target){
+                     return new int[] { x, y };
+                }
+            }
+        }
+        return null;
+    }
+}
